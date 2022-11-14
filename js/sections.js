c508d2e11e56deb907e10b875d51ed1f6679f81f
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-11-14 17:51:41 +0100

    section js

---
+++ js/sections.js
@@ -0,0 +1,32 @@
+// helper for HTML elements creation
+const createChildElement = (parentElement, element, className) => {
+  const childElement = document.createElement(element)
+  parentElement.appendChild(childElement)
+  childElement.classList.add(className)
+}
+// titles
+const titles = ["Me", "Skills", "Education", "Work", "Contact"]
+
+const skills = document.querySelector("#skills .sectionArticle")
+console.log(skills)
+const skillsIconsCoding = [
+  { image: "HTMLicon.png" },
+  { image: "CSSicon.png" },
+  { image: "CSSFLEXicon.png" },
+  { image: "CSSGRIDicon.png" },
+  { image: "SASSicon.png" },
+  { image: "JSicon.png" },
+  { image: "REACTicon.png" },
+  { image: "REDUXicon.png" },
+  { image: "jQueryicon.png" },
+  { image: "GITicon.png" },
+  { image: "JSONicon.png" },
+]
+for (let i = 0; i < skillsIconsCoding.length; i++) {
+  createChildElement(skills, "img", "skillsIcon")
+}
+let iconCoding = skills.querySelectorAll("img")
+console.log(iconCoding)
+for (let i = 0; skillsIconsCoding.length; i++) {
+  iconCoding[i].src = `img/icons/${skillsIconsCoding[i].image}`
+}
